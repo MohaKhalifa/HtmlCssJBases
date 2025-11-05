a96d754986502bd3dc99947c4835f14b48e9e0d9
mohakhalifa <04mohamedkhalifa04@gmail.com>
2025-11-05 02:07:19 +0100

    11/05/2025 02:07:19

---
+++ index.js
@@ -0,0 +1,9 @@
+//DOM:
+// html tags depend on js. or just modify them via DOM, refrence them;
+// Libraries: React, etc...
+//Brag: W3C WAI iniciative:standerd;requirment >Made ARIA tools for it;<screen readers;Speech recognition..  subtitiles get libs;  > accessability: ...... for people with disabilities;
+//
+//
+//
+//
+//
